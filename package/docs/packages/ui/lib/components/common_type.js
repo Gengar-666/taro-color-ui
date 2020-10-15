780dfbd06040e25a943344a7b0f9544a4e1b5efe
obito <lvy.123@foxmail.com>
2020-10-15 19:45:24 +0800

    Update Document

--- package/docs/packages/ui/lib/components/common_type.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=common_type.js.map
--- packages/ui/types/components/list/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ListItem from "./item";
-import { CommonProps } from "../types/common";
-interface IProps extends CommonProps {
-}
-export declare const CuList: React.FC<IProps> & {
-    Item: typeof ListItem;
-};
-export default CuList;
